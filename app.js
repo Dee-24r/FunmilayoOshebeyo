3b4342b9e07fd54c97a72c96259a8f67062e52cf
Funmilayo <fummyoshe@gmail.com>
2026-05-23 22:51:17 -0400

    minor change

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function typeWriter(target, text, speed = 50, onComplete = null){
     }
 }
 
-const portfText = "Tech Girl ● Adventurous spirit ● Always Learning";
+const portfText = "Tech Girl ● Adventurous ● Always Learning";
 
 const introText = "Hi, I'm Funmilayo! I love technology, afrobeats, and apple juice.";
 const welcome = "Welcome to my world!";
